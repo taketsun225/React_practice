81771f7fd7f2d9c918a2fe5455718ea8d45bdb49
taketsun225 <96859207+taketsun225@users.noreply.github.com>
2022-02-06 13:48:56 +0900

    Update

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,11 +1,9 @@
-const colorfulMessage = (props) => {
+export const ColorfulMessage = (props) => {
   const { color, children } = props;
   const contentStyle = {
-    ConvolverNode,
+    color,
     fontSize: "18px"
   };
 
   return <p style={contentStyle}>{children}</p>;
 };
-
-export default colorfulMessage;
